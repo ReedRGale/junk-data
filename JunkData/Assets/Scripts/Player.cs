882db1474d445859a027a2ca066dda50df274fe8
Reed Gale <jadersleet@gmail.com>
2018-04-22 01:06:13 -0700

    Semi-Functional Prediction Tool
    Built a semi functioning prediction tool to help with detecting edges.

--- JunkData/Assets/Scripts/Player.cs
+++ JunkData/Assets/Scripts/Player.cs
@@ -10,36 +10,38 @@ public class Player : MonoBehaviour
     public float jumpForce;
     public float gravityBalance;
     public GameObject marker;
+    public bool grounded;
 
     private Vector3 mouseDir;
     private int horizontal;
     private float mouseAngle;
-    private bool grounded;
     private bool grasping;
     private bool leftClickUp;
-    private bool stopRight;
-    private bool stopLeft;
     private Rigidbody2D rb2d;
+    private PredictionTool prediction;
     
     private Vector2 NUDGE_LEFT = new Vector2(-0.05f, 0);
     private Vector2 NUDGE_RIGHT = new Vector2(0.05f, 0);
     private const int LEFT_CLICK = 0;
-    private const float DETECTION_RANGE = 0.05f;
+    private const float DETECTION_RANGE = 0.03f;
     private const float STOP_MOVEMENT = 45f;
     private const float RIGHT_ANGLE = 90f;
-    private const float WALLDETECT_ANGLE_RESTRICTION = 25f;
+    private const float WALLDETECT_ANGLE_RESTRICTION = 25f;     // Degrees from 90 at which we're seeing a wall.
     private const float WALLSTICK_ANGLE_RESTRICTION = 8f;
-    private const float FLOORSTICK_ANGLE_RESTRICTION = 60f;
+    private const float CLIMB_CORRECTION = 0.5f;
 
     private void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        prediction = gameObject.GetComponent<PredictionTool>();
     }
 
     private void Update()
     {
         // Check for walk input.
         horizontal = (int)Input.GetAxisRaw("Horizontal");
+        if (horizontal == -1 && prediction.leftLocked || horizontal == 1 && prediction.rightLocked)
+            horizontal = 0;
 
         // Check for jump input.
         leftClickUp = !leftClickUp && grounded ? Input.GetMouseButtonUp(LEFT_CLICK) : leftClickUp;
@@ -59,48 +61,39 @@ public class Player : MonoBehaviour
         // Don't allow movement input if not grounded.
         if (!grounded) return;
 
-        // We're moving, so gravity must apply.
-        rb2d.gravityScale = 1.0f;
-
         // Move.
         if (leftClickUp && rb2d.velocity.magnitude < float.Epsilon)
         {
             rb2d.AddForce(mouseDir * jumpForce, ForceMode2D.Impulse);
             leftClickUp = false;
         }
-        else if (System.Math.Abs(horizontal) > 0 && rb2d.velocity.magnitude < walkVelocity)
+        else if (Mathf.Abs(horizontal) > 0 && rb2d.velocity.magnitude < walkVelocity)
         {
-            if (rb2d.velocity.x * horizontal < 0) rb2d.AddForce(-rb2d.velocity * heelTurn);
-            else
-            {
-                // Cast a prediction of the model in front of the movement.
-                RaycastHit2D[] lowBarrier = new RaycastHit2D[1];
-                rb2d.Cast(Vector2.right * horizontal, lowBarrier, DETECTION_RANGE);
+            // Cast a prediction of the model in front of the movement.
+            RaycastHit2D[] lowBarrier = new RaycastHit2D[1];
+            rb2d.Cast(Vector2.right * horizontal, lowBarrier, DETECTION_RANGE);
 
-                // Use the collider points from the cast model to determine force angle.
-                Vector2 collisionDir = (lowBarrier[0].point - lowBarrier[0].centroid);
-                collisionDir = Vector2.zero.Equals(collisionDir) ? Vector2.down : collisionDir; 
-                Vector2 moveDir = Rotate(collisionDir, RIGHT_ANGLE * horizontal).normalized;
+            // Use the collider points from the cast model to determine force angle.
+            Vector2 collisionDir = (lowBarrier[0].point - lowBarrier[0].centroid);
+            collisionDir = Vector2.zero.Equals(collisionDir) ? Vector2.down : collisionDir;
+            Vector2 moveDir = Rotate(collisionDir, RIGHT_ANGLE * horizontal).normalized;
 
-                // Restricted angles so we don't start moving up at walls.
-                float highRestriction = WALLDETECT_ANGLE_RESTRICTION + RIGHT_ANGLE;
-                float lowRestriction = RIGHT_ANGLE - WALLDETECT_ANGLE_RESTRICTION;
-                float correctionAngle = Vector2.Angle(Vector2.right, moveDir);
+            // Restricted angles so we don't start moving up at walls.
+            float highRestriction = WALLDETECT_ANGLE_RESTRICTION + RIGHT_ANGLE;
+            float lowRestriction = RIGHT_ANGLE - WALLDETECT_ANGLE_RESTRICTION;
+            float correctionAngle = Vector2.Angle(Vector2.right, moveDir);
 
-                // Correct for gravity.
-                float ungravity = moveDir.y * gravityBalance;
-                moveDir.y = ungravity;
+            // Correct for gravity.
+            float ungravity = moveDir.y * gravityBalance + CLIMB_CORRECTION;
+            moveDir.y = ungravity;
 
-                // Move, using the given prediction.
-                if (stopLeft)
-                    rb2d.AddForce(NUDGE_RIGHT);
-                if (stopRight)
-                    rb2d.AddForce(NUDGE_LEFT);
-                if (correctionAngle <= lowRestriction || correctionAngle >= highRestriction)
-                    rb2d.AddForce(moveDir * walkAccel);
-            }
+            // Normalize right and left movement.
+            moveDir.x = horizontal;
+
+            // Move, using the given prediction.
+            if (correctionAngle <= lowRestriction || correctionAngle >= highRestriction)
+                rb2d.AddForce(moveDir * walkAccel);
         }
-        else rb2d.AddForce(-rb2d.velocity);
     }
 
     void OnCollisionStay2D(Collision2D other)
@@ -111,26 +104,19 @@ public class Player : MonoBehaviour
         Vector2 collisionDir = (contact[0].point - (Vector2)rb2d.transform.position);
         float collisionAngle = Vector2.Angle(Vector2.right, collisionDir);
 
+        // Check if colliding with a wall.
         if (collisionAngle < WALLSTICK_ANGLE_RESTRICTION)
             rb2d.AddForce(NUDGE_LEFT);
         else if (collisionAngle > 180f - WALLSTICK_ANGLE_RESTRICTION)
             rb2d.AddForce(NUDGE_RIGHT);
-        else if (collisionAngle < STOP_MOVEMENT)
-        {
-            Debug.Log("stopLeft");
-            stopLeft = true;
-        }
-        else if (collisionAngle > 180f - STOP_MOVEMENT)
-        {
-            Debug.Log("stopRight");
-            stopRight = true;
-        }
-        else if (collisionAngle > FLOORSTICK_ANGLE_RESTRICTION || collisionAngle < 180f - FLOORSTICK_ANGLE_RESTRICTION)
-        {
-            stopLeft = false;
-            stopRight = true;
+        else
+            // Otherwise, we're grounded safely.
             grounded = true;
-        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
     }
 
     private Vector2 Rotate(Vector2 v, float degrees)
---
+++ JunkData/Assets/Scripts/PredictionTool.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PredictionTool : MonoBehaviour
+{
+    public bool rightLocked;
+    public bool leftLocked;
+    [HideInInspector] public bool wouldDie;                     // Is 'death' a piece of data that means something?
+    [HideInInspector] public bool wouldFall;                    // Is 'fall' a piece of data that means something?
+
+    private int direction;
+    private RaycastHit2D predeath = new RaycastHit2D();         // The last safe point before the next potential threat.
+    private RaycastHit2D postdeath = new RaycastHit2D();         // The last safe point before the next potential threat.
+    private RaycastHit2D prefall = new RaycastHit2D();          // The last safe point before the next potential fall.
+    private RaycastHit2D postfall = new RaycastHit2D();          // The last safe point before the next potential fall.
+    private Player player;
+    private float width;                                        // The width of the collider.
+    private Rigidbody2D rb2d;
+
+    public const int PREDICTION_SLICES = 10;                    // How many slices to cut the lookahead data into.
+
+    // CHANGE THIS TO SCALE WITH RADIUS
+    private const float EXTREME_CHANGE = 0.01f;                 // Units between two height values before we can presume a fall.
+
+    void Start()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        width = gameObject.GetComponent<Collider2D>().bounds.extents.x * 2;
+        player = gameObject.GetComponent<Player>();
+    }
+
+    void FixedUpdate()
+    {
+        // If the player isn't moving and is grounded, we don't want to undo any settings.
+        if (Mathf.Abs(rb2d.velocity.x) < float.Epsilon || !player.grounded) return;
+
+        // Reset bools.
+        wouldDie = false;
+        wouldFall = false;
+
+        // Collect and analyze data from raycasts.
+        SetDirectionLocks(CastRays(2 * StoppingDistance(), PREDICTION_SLICES), CastRays(2 * (StoppingDistance() + width), PREDICTION_SLICES));
+    }
+
+    // Collect data ahead of the unit.
+    private RaycastHit2D[] CastRays(float predictionDistance, float slices)
+    {
+        // Slice up prediction line into even segments to create origin points for our detection lines.
+        float pOriginX = predictionDistance / slices;
+
+        // Prepare an array to hold all RaycastHits.
+        RaycastHit2D[] data = new RaycastHit2D[PREDICTION_SLICES];
+
+        // Use origins as x values to generate a line of points to cast rays from.
+        for (int i = 0; i < PREDICTION_SLICES; i++)
+        {
+            data[i] = Physics2D.Raycast(new Vector2(rb2d.position.x + (MoveDir() * pOriginX * i), rb2d.position.y), Vector2.down);
+
+            // DEBUG:
+            Debug.DrawRay(new Vector2(rb2d.position.x + (MoveDir() * pOriginX * i), rb2d.position.y), Vector2.down, Color.cyan, 0.05f);
+        }
+
+        // Cast rays and return them in an array.
+        return data;
+    }
+
+    // Take the data collected and set locking information.
+    private void SetDirectionLocks(RaycastHit2D[] data, RaycastHit2D[] lookahead)
+    {
+        // LookForDanger(lookahead);  // Needs testing.
+        LookForFall(data, lookahead);
+        DirectionLock();     
+    }
+    
+    // If there's an object that might kill you up ahead, sets wouldDie and the points before and after death.
+    private void LookForDanger(RaycastHit2D[] data)
+    {
+        wouldDie = false;
+        int i = 0;
+        for (i = 0; !wouldDie && i < data.Length; i++)
+        {
+            wouldDie = data[i].collider.tag.Equals("DangerZone");
+            if (wouldDie)
+            {
+                predeath = data[i - 1];
+                postdeath = data[i];
+                return;
+            }
+        }
+    }
+
+    // Lookahead for whether you'll fall or not.
+    private void LookForFall(RaycastHit2D[] data, RaycastHit2D[] lookahead)
+    {
+        // Analyze data given and convert it to useful forms.
+        float[] stoppingDeltas = AnalyzeHeightData(data);
+        int pointsToPlayerWidth = AnalyzeFallLookahead(lookahead);
+
+        Debug.Log(data[0].point.x - data[1].point.x);
+
+        // Check for potential falls.
+        for (int i = 0; i < stoppingDeltas.Length; i++)
+            if (stoppingDeltas[i] > EXTREME_CHANGE)
+            {
+                wouldFall = true;
+                prefall = data[i];
+                postfall = data[i + 1];
+                break;
+            }
+
+        // Simulate the shape beyond an extreme angle to determine if the unit would fall into it.
+        if (wouldFall)
+        {
+            // Get the first point in the lookahead that's within extreme change range.
+            int lookaheadFall = 0;
+            for (int i = 0; i < lookahead.Length; i++)
+                if (MoveDir() < 0 && postfall.point.x > lookahead[i].point.x || MoveDir() > 0 && postfall.point.x < lookahead[i].point.x)
+                {
+                    lookaheadFall = i;
+                    break;
+                }
+
+            // Look beyond the unit's width beyond the fall point.
+            for (int i = 0; i < pointsToPlayerWidth || lookaheadFall + i < lookahead.Length; i++)
+                // If the data says there are any points we experience where an extreme change isn't registered, we're not in danger of falling.
+                if (prefall.point.y - lookahead[lookaheadFall + i].point.y < EXTREME_CHANGE)
+                {
+                    wouldFall = false;
+                    break;
+                }
+        }
+    }
+
+    // Return an array of changes in heights between the collision points.
+    private float[] AnalyzeHeightData(RaycastHit2D[] data)
+    {
+        float[] deltaHeight = new float[data.Length - 1];
+        for (int i = 0; i < data.Length - 1; i++)
+            deltaHeight[i] = data[i].point.y - data[i + 1].point.y;
+        return deltaHeight;
+    }
+
+    // Return the length just shy of the player's length in prediction slices.
+    private int AnalyzeFallLookahead(RaycastHit2D[] lookahead)
+    {
+        // Get the x value between any two points.
+        float deltaX = lookahead[0].point.x - lookahead[1].point.x;
+
+        // Return the width of the player divided by delta x rounded down.
+        return (int)Mathf.Abs(Mathf.Floor(width / deltaX));
+    }
+
+    // Determine which directions should be locked.
+    private void DirectionLock()
+    {
+        if (wouldFall || wouldDie)
+        {
+            // If the fall point's x magnitude outstrips the danger point's magnitude...
+            RaycastHit2D closest = Mathf.Abs(rb2d.position.x - postfall.point.x) < Mathf.Abs(rb2d.position.x - postdeath.point.x) ? postfall : postdeath;
+
+            // Determine which direction to lock movement.
+            if (rb2d.position.x - closest.point.x > 0)
+            {
+                leftLocked = true;
+                rightLocked = false;
+            }
+            else
+            {
+                leftLocked = false;
+                rightLocked = true;
+            }
+        }
+        else if (rb2d.velocity.x > 0.1f && leftLocked)
+        {
+            // Debug.Log("Left Unlocked X: " + rb2d.velocity.x);
+            leftLocked = false;
+        }
+        else if (rb2d.velocity.x < -0.1f && rightLocked)
+        {
+            // Debug.Log("Right Unlocked X: " + rb2d.velocity.x);
+            rightLocked = false;
+        }
+        else if (rb2d.velocity.y > 2)
+        {
+            leftLocked = false;
+            rightLocked = false;
+        }
+    }
+
+    // Calculate the stopping distance for our prediction. [KE = (1/2)mv^2]
+    private float StoppingDistance() { return 0.5f * rb2d.mass * Mathf.Pow(rb2d.velocity.x, 2); }
+
+    // Helper function to determine the direction of the player.
+    // 0:  Not Moving
+    // 1:  Moving Right
+    // -1: Moving Left
+    private int MoveDir() { return rb2d.velocity.x < 0 ? -1 : 1; }
+}
